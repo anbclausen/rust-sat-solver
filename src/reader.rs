d02002353b56af64df0e8082555c55a7f532bae1
Anders Benjamin Clausen <anbclausen@gmail.com>
2023-07-18 17:39:40 +0200

    refactor: better typing

--- src/reader.rs
+++ src/reader.rs
@@ -1,4 +1,4 @@
-use crate::types::Problem;
+use crate::types::{Literal, Clause, Problem};
 use std::fs::read_to_string;
 
 fn read_file_to_string(path: &str) -> String {
@@ -9,15 +9,15 @@ pub fn read_file_to_cnf(path: &str) -> Problem {
     let contents = read_file_to_string(path);
     let lines: Vec<&str> = contents.lines().collect();
     let header = lines[0];
-    let (num_vars, _) = scan_fmt!(header, "p cnf {} {}", i32, i32)
+    let (num_vars, _) = scan_fmt!(header, "p cnf {} {}", Literal, usize)
         .unwrap_or_else(|_| panic!("Could not parse header of file '{path}'."));
 
     let clauses = lines[1..].iter().map(|line| {
         line.split_whitespace()
-            .map(|s| s.parse::<i32>().unwrap())
+            .map(|s| s.parse::<Literal>().unwrap())
             .take_while(|&n| n != 0)
-            .collect::<Vec<i32>>()
-    }).collect();
+            .collect::<Clause>()
+    }).collect::<Vec<Clause>>();
 
     Problem { num_vars, clauses }
 }
