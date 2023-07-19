e98c732fbad50e68d8b8ae4a5d7c398d570a41a6
Anders Benjamin Clausen <anbclausen@gmail.com>
2023-07-19 23:19:37 +0200

    feat: added tests to sanity check solution

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,45 @@ fn main() {
     let result = naive(&cnf);
     print!("{:?}", result)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::reader::read_file_to_cnf;
+    use crate::solver::naive;
+    use crate::types::Res;
+
+    #[test]
+    fn trivial_should_be_satisfiable() {
+        let cnf = read_file_to_cnf("tests/trivial.cnf");
+        let result = naive(&cnf);
+        assert!(matches!(result, Res::Satisfiable ( .. )))
+    }
+
+    #[test]
+    fn false_should_be_unsatisfiable() {
+        let cnf = read_file_to_cnf("tests/false.cnf");
+        let result = naive(&cnf);
+        assert!(matches!(result, Res::Unsatisfiable))
+    }
+
+    #[test]
+    fn full4_should_be_unsatisfiable() {
+        let cnf = read_file_to_cnf("tests/full4.cnf");
+        let result = naive(&cnf);
+        assert!(matches!(result, Res::Unsatisfiable))
+    }
+
+    #[test]
+    fn ph2_should_be_unsatisfiable() {
+        let cnf = read_file_to_cnf("tests/ph2.cnf");
+        let result = naive(&cnf);
+        assert!(matches!(result, Res::Unsatisfiable))
+    }
+
+    #[test]
+    fn prime4_should_be_satisfiable() {
+        let cnf = read_file_to_cnf("tests/prime4.cnf");
+        let result = naive(&cnf);
+        assert!(matches!(result, Res::Satisfiable ( .. )))
+    }
+}
